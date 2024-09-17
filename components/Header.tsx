6acb531db533dfa92429cb87adc4faa6e52699a1
nishanthan-k <krn.nishanthan@gmail.com>
2024-09-17 19:16:59 +0530

    feat: header

---
+++ components/Header.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Logo from './Logo';
+import { Menu } from './NavigationMenu';
+import { SearchInput } from './ui/SearchInput';
+import CartIcon from '@/assets/icons/cart.webp';
+import UserIcon from '@/assets/icons/profile.webp';
+import SearchIcon from '@/assets/icons/searchDark.webp'
+import Image from 'next/image';
+import { MenuSheet } from './MenuSheet';
+
+const Header = () => {
+  return (
+    <header className='shadow-sm py-2 sm:px-8 sm:py-4 flex items-center sm:flexCenter sm:gap-8'>
+      <div className='sm:hidden iconButton'>
+        <MenuSheet />
+      </div>
+
+      <Logo />
+
+      <nav className='hidden sm:flex'>
+        <Menu />
+        <ul className='flex gap-6 px-4 py-2 text-sm font-medium transition-colors hover:text-accent-foreground focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50'>
+          <li className='cursor-pointer text-nowrap'>On Sale</li>
+          <li className='cursor-pointer text-nowrap'>New Arrival</li>
+        </ul>
+      </nav>
+
+      <SearchInput
+        placeholder='Search for products'
+        className='w-1/2 hidden sm:block'
+        // onChange={() => {}}
+      />
+
+      <div className='flex ml-auto gap-2 '>
+        <div className='sm:hidden iconButton'>
+          <Image src={SearchIcon} width={12} height={12} alt='icon' />
+        </div>
+
+        <div className='iconButton'>
+          <Image src={CartIcon} width={12} height={12} alt='icon' />
+        </div>
+
+        <div className='iconButton'>
+          <Image src={UserIcon} width={14} height={14} alt='icon' />
+        </div>
+      </div>
+
+    </header>
+  )
+}
+
+export default Header;
---
+++ components/MenuSheet.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import MenuBar from '@/assets/icons/menuBar.webp'
+import { Button } from "@/components/ui/button"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetTitle,
+  SheetTrigger,
+  SheetDescription,
+} from "@/components/ui/sheet"
+import Image from "next/image"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion'
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
+
+export function MenuSheet() {
+  const menuList = ["Link 1", "Link 2", "Link 3", "Link 4"];
+  
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Image src={MenuBar} width={12} height={12} alt='icon' priority />
+        </SheetTrigger>
+        <SheetContent side="left" className='bg-white'>
+          <div className="grid gap-4 py-4">
+            <VisuallyHidden>
+              <SheetTitle>Menu</SheetTitle>
+            <SheetDescription>
+              Nav links
+            </SheetDescription>
+            </VisuallyHidden>
+            <nav className='sm:hidden'>
+              <Accordion type="single" collapsible className="w-full flexCenter">
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>Shop</AccordionTrigger>
+                  {menuList.map((item) => (
+                    <AccordionContent key={item}>{item}</AccordionContent>
+                  ))}
+                </AccordionItem>
+              </Accordion>
+
+              <ul className='flexCenter flex-col gap-6 px-4 py-2 text-sm font-medium transition-colors hover:text-accent-foreground focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50'>
+                <li className='cursor-pointer text-nowrap'>On Sale</li>
+                <li className='cursor-pointer text-nowrap'>New Arrival</li>
+              </ul>
+            </nav>
+          </div>
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit">Login</Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </div>
+  )
+}
