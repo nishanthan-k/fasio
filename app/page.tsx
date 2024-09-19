b5fe0bd64261d4a6c0c187f7c69d31a93d3df7fa
nishanthan-k <krn.nishanthan@gmail.com>
2024-09-19 10:48:26 +0530

    feat: brand carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { BrandCarousel } from "@/components/BrandCarousel";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
       <Hero />
+      <BrandCarousel />
     </div>
   );
 }
